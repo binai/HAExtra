a23ea236e540c6367ccd573f26c0a85aa8728c4a
Yonsm <I@YONSM.NET>
2018-08-15 23:13:52 +0800

    refine

--- hagenie/miai.py
+++ hagenie/miai.py
@@ -21,20 +21,26 @@ if REQUEST_METHOD:
     log(REQUEST_METHOD + ' ' + os.environ['SCRIPT_NAME'] + '?' + os.environ['QUERY_STRING'] + '\n')
 
 #
-_appName = None
+_appName = '小爱精灵'
 _haUrl = None
 def validateToken(queryString):
+    global _appName
+    global _haUrl
     parts = queryString.split('_')
-    if len(parts) > 3 and (parts[1] == 'http' or parts[1] == 'https'):
-        global _appName
-        global _haUrl
+    count = len(parts)
+    if count > 2 and (parts[0] == 'http' or parts[0] == 'https'):
+        i = 0
+    elif count > 3 and (parts[1] == 'http' or parts[1] == 'https'):
         _appName = unquote(parts[0])
-        _haUrl = parts[1] + '://' + parts[2] + ':' + parts[3] + '/api/%s'
-        if len(parts) > 4:
-            _haUrl += '?api_password=' + parts[4]
-        log('validateToken: ' + _appName + ', HAURL: ' + _haUrl)
-        return True
-    return False
+        i = 1
+    else:
+        return False
+
+    _haUrl = parts[i] + '://' + parts[i+1] + ':' + parts[i+2] + '/api/%s'
+    if count > i+3:
+        _haUrl += '?api_password=' + parts[i+3]
+    log('validateToken: ' + _appName + ', HAURL: ' + _haUrl)
+    return True
 
 #
 def haCall(cmd, data=None):
@@ -45,12 +51,26 @@ def haCall(cmd, data=None):
         log(data)
     if url.startswith('https'): # We need extra requests lib for HTTPS POST
         import requests
-        result = requests.request(method, url, data=data, timeout=3).text
+        result = requests.request(method, url, data=data, timeout=12).text
     else:
         result = urlopen(url, data=data, timeout=3).read()
 
     #log('HA RESPONSE: ' + result)
     return json.loads(result)
+
+#
+def guessAction(entity_id, intent_name, query):
+    if not entity_id.startswith('sensor') and not entity_id.startswith('binary_sensor') and not entity_id.startswith('device_tracker'):
+        # 使用传入唤醒词截断
+        if _appName != '小爱精灵':
+            query = query.split(_appName)[-1]
+
+        # 使用小米意图识别或做意图识别
+        if intent_name == 'open' or query.startswith('打开') or query.startswith('开'):
+            return '打开'
+        elif intent_name == 'close' or query.startswith('关'):
+            return '关闭'
+    return '查询'
 
 #
 STATE_NAMES = {
@@ -73,45 +93,19 @@ STATE_NAMES = {
 
     'unavailable': '不可用',
 }
-
 #
-def handleState(state):
-    return '为' + STATE_NAMES[state] if state in STATE_NAMES else state
-
-#
-def handleEntity(entity_id, item, query):
-    domain = entity_id[:entity_id.find('.')]
-    state = item['state']
-    if domain == 'sensor' and domain == 'binary_sensor' and domain == 'device_tracker':
-        return handleState(state)
-
-    if query.startswith('打开') or query.startswith('开'):
-        action = '打开'
-        service = 'open_cover' if domain == 'cover' else 'turn_on'
-    elif query.startswith('关'):
-        action = '关闭'
-        service = 'close_cover' if domain == 'cover' else 'turn_off'
+def handleEntity(entity_id, state, action):
+    #domain = entity_id[:entity_id.find('.')]
+    if action == '打开':
+        service = 'turn_on' # if domain != 'cover' else 'open_cover'
+    elif action == '关闭':
+        service = 'turn_off' # if domain != 'cover' else 'close_cover'
     else:
-        return handleState(state)
+        return '为' + (STATE_NAMES[state] if state in STATE_NAMES else state)
 
     data = '{"entity_id":"' + entity_id + '"}'
-    result = haCall('services/' + domain + '/' + service, data)
+    result = haCall('services/homeassistant/' + service, data)
     return action + "成功" if type(result) is list else "不成功"
-
-#
-def handleQuery(query, request_type):
-    items = haCall('states')
-    for item in items:
-        entity_id = item['entity_id']
-        if entity_id.startswith('group'):
-            continue
-
-        attributes = item['attributes']
-        friendly_name = attributes.get('friendly_name')
-        if friendly_name is not None and query.endswith(friendly_name):
-            return (True, friendly_name + handleEntity(entity_id, item, query.split(_appName)[-1]))
-
-    return (False, "您好主人，我能为你做什么呢？" if (request_type == 0 and query.endswith(_appName)) else _appName + "未找到设备，请再说一遍吧")
 
 #
 def handleRequest(body):
@@ -125,12 +119,29 @@ def handleRequest(body):
         return (False, '主人，您还在吗？')
 
     #
-    query = body['query']
     request_type = request['type']
     if request_type == 2:
         return (True, "再见主人，" + _appName + "在这里等你哦！")
 
-    return handleQuery(query, request_type)
+    #
+    query = body['query']
+    slot_info = body['request'].get('slot_info')
+    intent_name = slot_info.get('intent_name') if slot_info is not None else None
+
+    #
+    items = haCall('states')
+    for item in items:
+        entity_id = item['entity_id']
+        if entity_id.startswith('zone'):
+            continue
+
+        attributes = item['attributes']
+        friendly_name = attributes.get('friendly_name')
+        if friendly_name is not None and query.endswith(friendly_name):
+            action = guessAction(entity_id, intent_name, query)
+            return (True, friendly_name + handleEntity(entity_id, item['state'], action))
+
+    return (False, "您好主人，我能为你做什么呢？" if intent_name == 'Mi_Welcome' else _appName + "未找到设备，请再说一遍吧")
 
 # Main process
 try:
